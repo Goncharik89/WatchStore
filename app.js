37dca5bb9fbc9b9de4e2f14a64310ac8dc3e16bb
Nikita Krivosheyev <nik.krivosheyev@gmail.com>
2022-08-17 09:14:38 +0300

    static fix

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ const orderRouts = require('./routes/order');
 const app = express();
 
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, '../public')));
+// app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.resolve('public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
