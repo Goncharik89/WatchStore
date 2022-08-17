d0c386a30441ae48a6f238104f6aa57605c3a238
Nikita Krivosheyev <nik.krivosheyev@gmail.com>
2022-08-17 09:16:11 +0300

    Home view fix

--- views/Home.jsx
+++ views/Home.jsx
@@ -1,13 +1,17 @@
 /* eslint-disable react/prop-types */
 const React = require('react');
+const About = require('./About');
 const Catalog = require('./Catalog');
 const Layout = require('./Layout');
+const Order = require('./Order');
 
 module.exports = function Home({ newUser }) {
   return (
     <Layout newUser={newUser}>
       <h1>Hello!</h1>
+      <About />
       <Catalog />
+      <Order />
 
     </Layout>
   );
