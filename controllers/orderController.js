f0e2e4a650d7e72a7695d69a81636f8bac283f24
Alexandr Podovinnikov <the21room@yandex.ru>
2022-08-17 09:09:52 +0300

    order_multer

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,28 +1,31 @@
 const renderTemplate = require('../lib/renderTemplate');
-const Order = require('../views/Order');
+const OrderJSX = require('../views/Order');
+
+const { Watch, User, Order } = require('../db/models');
 
 exports.formOrder = (req, res) => {
   const name = req.session?.newUserName;
-  renderTemplate(Order, { username: name }, res);
+  renderTemplate(OrderJSX, { username: name }, res);
 };
 
 exports.newOrder = async (req, res) => { // multer создает req.file
   try {
-    const findUser = await User.findOne({ where: { id: req.session.user.id } }); // ищем юзера
-    const findWatch = await Watch.findOne({ where: { id: req.session.watch.id } });
-    const newOrder = await Order.create({ // создаем карту
+    // const findWatch = await Watch.findOne({ where: { id: req.session.watch.id } });
+    // const findUser = await User.findOne({ where: { id: req.session.newUser.id } }); // ищем юзера
+    await Order.create({
       // watch_id: findWatch.id,
       // user_id: findUser.id,
       name: req.body.name,
       email: req.body.email,
       phone: req.body.phone,
-      description: req.body.descritpion,
+      pic: req.file.filename,
+      description: req.body.description,
     }, {
       returning: true, // хз
       plain: true, // хз
     });
 
-    res.redirect(`/order`); // редирект на созданную карточку
+    res.redirect('/'); // редирект на созданную карточку
   } catch (error) {
     res.send(error.message);
   }
