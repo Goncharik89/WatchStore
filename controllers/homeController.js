a95e31708a5e83351b216277b30faf6ec1515fad
Nikita Krivosheyev <nik.krivosheyev@gmail.com>
2022-08-17 13:09:22 +0300

    homecontroller try catch added

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,9 +1,16 @@
 const renderTemplate = require('../lib/renderTemplate');
 const Home = require('../views/Home');
+const { Watch } = require('../db/models/watch');
 
-const renderHome = (req, res) => {
-  const newUser = req.session?.newUser;
-  renderTemplate(Home, { newUser }, res);
+const renderHome = async (req, res) => {
+  try {
+    const watch = await Watch.findAll();
+    console.log(watch);
+    const newUser = req.session?.newUser;
+    renderTemplate(Home, { newUser, watch }, res);
+  } catch (error) {
+    res.status(500).json({ errorMessage: error.message });
+  }
 };
 
 module.exports = renderHome;
